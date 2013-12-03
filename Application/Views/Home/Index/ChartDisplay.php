a5bedca762ac4781a7534fac8dad2c1867eebb55
james <james@JD-PC.home.local>
2013-12-03 17:18:15 +0000

    Cleaning up view.

--- Application/Views/Home/Index/ChartDisplay.php
+++ Application/Views/Home/Index/ChartDisplay.php
@@ -3,16 +3,34 @@
         <h3 class="panel-title">Crimes By Region</h3>
     </div>
     <div class="panel-body">
-        <ul class="nav nav-tabs">
-            <li><a href="#region-bar" data-toggle="tab" class="active">Bar</a></li>
-            <li><a href="#region-pie" data-toggle="tab">Pie</a></li>
-        </ul>
-        <div class="tab-content">
-            <div class="tab-pane active" id="region-bar">
-                <div class="alert alert-warning">Data loading</div>
+        <div class="row">
+            <div class="col-lg-6">
+                <h3>Data</h3>
             </div>
-            <div class="tab-pane" id="region-pie">
-                <div class="alert alert-warning">Data loading</div>
+            <div class="col-lg-6">
+                <h3>Charts</h3>
+                <ul class="nav nav-tabs">
+                    <li><a href="#region-bar" data-toggle="tab" class="active">Bar</a></li>
+                    <li><a href="#region-pie" data-toggle="tab">Pie</a></li>
+                </ul>
+                <div class="tab-content">
+                    <div class="tab-pane active" id="region-bar">
+                        <div class="row">
+                            <div class="row">&nbsp;</div>
+                            <div class="col-lg-12">
+                                <div class="alert alert-warning">Data loading</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab-pane" id="region-pie">
+                        <div class="row">&nbsp;</div>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="alert alert-warning">Data loading</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -22,16 +40,34 @@
         <h3 class="panel-title">Crimes By Area</h3>
     </div>
     <div class="panel-body">
-        <ul class="nav nav-tabs">
-            <li><a href="#area-bar" data-toggle="tab" class="active">Bar</a></li>
-            <li><a href="#area-pie" data-toggle="tab">Pie</a></li>
-        </ul>
-        <div class="tab-content">
-            <div class="tab-pane active" id="area-bar">
-                <div class="alert alert-warning">Data loading</div>
+        <div class="row">
+            <div class="col-lg-6">
+                <h3>Data</h3>
             </div>
-            <div class="tab-pane" id="area-pie">
-                <div class="alert alert-warning">Data loading</div>
+            <div class="col-lg-6">
+                <h3>Charts</h3>
+                <ul class="nav nav-tabs">
+                    <li><a href="#region-bar" data-toggle="tab" class="active">Bar</a></li>
+                    <li><a href="#region-pie" data-toggle="tab">Pie</a></li>
+                </ul>
+                <div class="tab-content">
+                    <div class="tab-pane active" id="region-bar">
+                        <div class="row">&nbsp;</div>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="alert alert-warning">Data loading</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab-pane" id="region-pie">
+                        <div class="row">&nbsp;</div>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="alert alert-warning">Data loading</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
