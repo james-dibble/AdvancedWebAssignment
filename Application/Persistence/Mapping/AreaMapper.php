17637f0a8ee9ad9a216913d0186f3e6d6c4339c0
j3-dibble <j3-dibble@STU-CZC32353JC.campus.ads.uwe.ac.uk>
2013-12-06 12:35:01 +0000

    Added all the crime stats columns.

--- Application/Persistence/Mapping/AreaMapper.php
+++ Application/Persistence/Mapping/AreaMapper.php
@@ -1,8 +1,10 @@
 <?php
+
 namespace Application\Persistence\Mapping;
 
 class AreaMapper implements \Library\Persistence\IMapper
 {
+
     public function GetAddQueries($objectToSave)
     {
         
@@ -15,19 +17,41 @@ class AreaMapper implements \Library\Persistence\IMapper
 
     public function GetFindQuery(\Library\Persistence\IPersistenceSearcher $searcher)
     {
-        $query = 
-                'SELECT `gr`.`id`, `gr`.`name`, `cs`.`Homicide`, `cs`.`ViolenceWithInjury` FROM
-    `area` `a`
-  INNER JOIN `geographicreference` `gr`
-    ON `gr`.`id` = `a`.`GeographicReference_Id`
-  INNER JOIN `crimestatistics` `cs`
-    ON `a`.`CrimeStatistics_Id` = `cs`.`id`';
-        
-        if($searcher->HasKey('ForRegion'))
+        $query =
+                'SELECT `gr`.`id`, `gr`.`name`, 
+                    `cs`.`Homicide`, 
+                    `cs`.`ViolenceWithInjury`,
+                    `cs`.`Homicide`,
+                    `cs`.`ViolenceWithInjury`,
+                    `cs`.`ViolenceWithoutInjury`,
+                    `cs`.`SexualOffenses`,
+                    `cs`.`Robbery`,
+                    `cs`.`TheftOffenses`,
+                    `cs`.`DomesticBurglary`,
+                    `cs`.`NonDomesticBurglary`,
+                    `cs`.`VehicleOffenses`,
+                    `cs`.`TheftFromPerson`,
+                    `cs`.`BicycleTheft`,
+                    `cs`.`Shoplifting`,
+                    `cs`.`MiscTheft`,
+                    `cs`.`CriminalDamageAndArson`,
+                    `cs`.`DrugOffenses`,
+                    `cs`.`PossesionOfWeapons`,
+                    `cs`.`PublicOrderOffenses`,
+                    `cs`.`MiscCrimes`,
+                    `cs`.`Fraud`
+                FROM
+                  `area` `a`
+                INNER JOIN `geographicreference` `gr`
+                  ON `gr`.`id` = `a`.`GeographicReference_Id`
+                INNER JOIN `crimestatistics` `cs`
+                  ON `a`.`CrimeStatistics_Id` = `cs`.`id`';
+
+        if ($searcher->HasKey('ForRegion'))
         {
             $query .= sprintf(' WHERE `a`.`Region_Id` = %d', $searcher->GetKey('ForRegion'));
         }
-        
+
         return $query;
     }
 
@@ -37,18 +61,36 @@ class AreaMapper implements \Library\Persistence\IMapper
     }
 
     public function MapObject($results)
-    {      
+    {
         $crimeStatistics = new \Application\Models\Domain\CrimeStatistics();
-                
+
         $crimeStatistics->homocide = $results->Homicide;
         $crimeStatistics->violenceWithInjury = $results->ViolenceWithInjury;
-        
+        $crimestatistics->violenceWithoutInjury = $results->ViolenceWithoutInjury;
+        $crimestatistics->sexualOffenses = $results->SexualOffenses;
+        $crimestatistics->robbery = $results->Robbery;
+        $crimestatistics->theftOffenses = $results->TheftOffenses;
+        $crimestatistics->domesticBurglary = $results->DomesticBurglary;
+        $crimestatistics->nonDomesticBurglary = $results->NonDomesticBurglary;
+        $crimestatistics->vehicleOffenses = $results->VehicleOffenses;
+        $crimestatistics->theftFromPerson = $results->TheftFromPerson;
+        $crimestatistics->bicycleTheft = $results->BicycleTheft;
+        $crimestatistics->shoplifting = $results->Shoplifting;
+        $crimestatistics->miscTheft = $results->MiscTheft;
+        $crimestatistics->criminalDamageAndArson = $results->CriminalDamageAndArson;
+        $crimestatistics->drugOffenses = $results->DrugOffenses;
+        $crimestatistics->possesionOfWeapons = $results->PossesionOfWeapons;
+        $crimestatistics->publicOrderOffenses = $results->PublicOrderOffenses;
+        $crimestatistics->miscCrimes = $results->MiscCrimes;
+        $crimestatistics->fraud = $results->Fraud;
+
         $mappedObject = new \Application\Models\Domain\Area($crimeStatistics);
-        
+
         $mappedObject->id = $results->name;
-        
+
         return $mappedObject;
-    }    
+    }
+
 }
 
 ?>
--- Application/Persistence/Mapping/NationalMapper.php
+++ Application/Persistence/Mapping/NationalMapper.php
@@ -16,7 +16,29 @@ class NationalMapper implements \Library\Persistence\IMapper
     public function GetFindQuery(\Library\Persistence\IPersistenceSearcher $searcher)
     {
         $query = 
-                'SELECT `gr`.`id`, `gr`.`name`, `cs`.`Homicide`, `cs`.`ViolenceWithInjury` FROM
+                'SELECT `gr`.`id`, `gr`.`name`, 
+                    `cs`.`Homicide`, 
+                    `cs`.`ViolenceWithInjury`,
+                    `cs`.`Homicide`,
+                    `cs`.`ViolenceWithInjury`,
+                    `cs`.`ViolenceWithoutInjury`,
+                    `cs`.`SexualOffenses`,
+                    `cs`.`Robbery`,
+                    `cs`.`TheftOffenses`,
+                    `cs`.`DomesticBurglary`,
+                    `cs`.`NonDomesticBurglary`,
+                    `cs`.`VehicleOffenses`,
+                    `cs`.`TheftFromPerson`,
+                    `cs`.`BicycleTheft`,
+                    `cs`.`Shoplifting`,
+                    `cs`.`MiscTheft`,
+                    `cs`.`CriminalDamageAndArson`,
+                    `cs`.`DrugOffenses`,
+                    `cs`.`PossesionOfWeapons`,
+                    `cs`.`PublicOrderOffenses`,
+                    `cs`.`MiscCrimes`,
+                    `cs`.`Fraud`
+                  FROM
                     `national` `n`
                   INNER JOIN `geographicreference` `gr`
                     ON `gr`.`id` = `n`.`GeographicReference_Id`
@@ -37,6 +59,23 @@ class NationalMapper implements \Library\Persistence\IMapper
                 
         $crimeStatistics->homocide = $results->Homicide;
         $crimeStatistics->violenceWithInjury = $results->ViolenceWithInjury;
+        $crimestatistics->violenceWithoutInjury = $results->ViolenceWithoutInjury;
+        $crimestatistics->sexualOffenses = $results->SexualOffenses;
+        $crimestatistics->robbery = $results->Robbery;
+        $crimestatistics->theftOffenses = $results->TheftOffenses;
+        $crimestatistics->domesticBurglary = $results->DomesticBurglary;
+        $crimestatistics->nonDomesticBurglary = $results->NonDomesticBurglary;
+        $crimestatistics->vehicleOffenses = $results->VehicleOffenses;
+        $crimestatistics->theftFromPerson = $results->TheftFromPerson;
+        $crimestatistics->bicycleTheft = $results->BicycleTheft;
+        $crimestatistics->shoplifting = $results->Shoplifting;
+        $crimestatistics->miscTheft = $results->MiscTheft;
+        $crimestatistics->criminalDamageAndArson = $results->CriminalDamageAndArson;
+        $crimestatistics->drugOffenses = $results->DrugOffenses;
+        $crimestatistics->possesionOfWeapons = $results->PossesionOfWeapons;
+        $crimestatistics->publicOrderOffenses = $results->PublicOrderOffenses;
+        $crimestatistics->miscCrimes = $results->MiscCrimes;
+        $crimestatistics->fraud = $results->Fraud;
         
         $mappedObject = new \Application\Models\Domain\National($crimeStatistics);
         
