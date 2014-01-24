e1705c12056b7a7910662af99a6cd8e4af626236
j3-dibble <j3-dibble@STU-CZC32353JC.campus.ads.uwe.ac.uk>
2014-01-24 12:51:26 +0000

    And code credits for client side libs.

--- Application/Views/Documentation/Index/Report.php
+++ Application/Views/Documentation/Index/Report.php
@@ -7,9 +7,80 @@
 </div>
 <div class="row">
     <div class="col-lg-12">
+        <h2>Credits</h2>
+        <p>
+        <table class="table table-bordered">
+            <tr>
+                <th>Plugin</th>
+                <th>Link</th>
+                <th>Usage</th>
+            </tr>
+            <tr>
+                <td><span class="label label-primary">AngularJS</span></td>
+                <td><a href="http://angularjs.org/" target="_blank"><code>http://angularjs.org/</code></a></td>
+                <td>
+                    <ul>
+                        <li>MVVM client side architecture</li>
+                        <li>AJAX request building and processing</li>
+                        <li>Data binding</li>
+                    </ul>
+                </td>
+            </tr>
+            <tr>
+                <td><span class="label label-primary">Bootstrap</span></td>
+                <td><a href="http://getbootstrap.com/" target="_blank"><code>http://getbootstrap.com/</code></a></td>
+                <td>
+                    <ul>
+                        <li>Client-side styling</li>
+                    </ul>
+                </td>
+            </tr>
+            <tr>
+                <td><span class="label label-primary">Readable Bootswatch Theme</span></td>
+                <td><a href="http://bootswatch.com/readable/" target="_blank"><code>http://bootswatch.com/readable/</code></a></td>
+                <td>
+                    <ul>
+                        <li>Client-side styling</li>
+                    </ul>
+                </td>
+            </tr>
+            <tr>
+                <td><span class="label label-primary">Highcharts</span></td>
+                <td><a href="http://www.highcharts.com/" target="_blank"><code>http://www.highcharts.com/</code></a></td>
+                <td>
+                    <ul>
+                        <li>Data visualisation</li>
+                    </ul>
+                </td>
+            </tr>
+            <tr>
+                <td><span class="label label-primary">LINQ JS</span></td>
+                <td><a href="http://linqjs.codeplex.com/" target="_blank"><code>http://linqjs.codeplex.com/</code></a></td>
+                <td>
+                    <ul>
+                        <li>Array querying</li>
+                        <li>JS object manipulation</li>
+                    </ul>
+                </td>
+            </tr>
+            <tr>
+                <td><span class="label label-primary">Repo.js</span></td>
+                <td><a href="http://darcyclarke.me/dev/repojs/" target="_blank"><code>http://darcyclarke.me/dev/repojs/</code></a></td>
+                <td>
+                    <ul>
+                        <li>Displaying code source from Github.</li>
+                    </ul>
+                </td>
+            </tr>
+        </table>
+        </p>
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-12">
         <h2>Code</h2>
         <p>
-            Code can be viewed on <a href="https://github.com/james-dibble/AdvancedWebAssignment">GitHub</a>.
+            Code repository hosted upon <a href="https://github.com/james-dibble/AdvancedWebAssignment">GitHub</a>.
         </p>
         <div id="repoBrowser"></div>
     </div>
