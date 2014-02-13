5ee0d174f6679be579d8b3f77d7a58697d9e9c34
j3-dibble <j3-dibble@STU-CZC32353JC.campus.ads.uwe.ac.uk>
2014-02-13 12:39:00 +0000

    Writing report.

--- Application/Views/Documentation/Index/Report.php
+++ Application/Views/Documentation/Index/Report.php
@@ -3,6 +3,50 @@
         <div class="page-header">
             <h1>Report</h1>
         </div>
+        <h3>Use of patterns</h3>
+        <p>
+            Several design patterns were utilised to build the application.  The overall architecture is based around 
+            service-orientation, where actions completed upon domain models are restricted to a layer of classes.  These
+            classes are also the only ones that have access to the persistence layer.  The domain model layer is shared
+            between all the layers of the application.
+        </p>
+        <p>
+            The presentation layer, relies upon an MVC pattern, where the controllers invoke services to gain information
+            about the models and build views or data responses.  HTTP actions are routed to appropriate controllers via
+            HTAccess entries and the sole "script" file in the application that invokes the routing framework.
+        </p>
+        <p>
+            Inversion of Control is the final core part of the architecture, where dependencies are defined before routing
+            is invoked and injected into controllers when they are constructed.  This helps apply the Dependency Inversion 
+            principle of <a href="http://en.wikipedia.org/wiki/SOLID" target="_blank">SOLID</a>
+            object oriented design.  If a <a href="http://en.wikipedia.org/wiki/Test-driven_development" target="_blank">TDD</a> 
+            development approach had been utilised, as the vast majority of dependencies are injected into classes rather 
+            than created, unit testing would have been much easier to complete as mock objects could have substituted.
+        </p>
+        <p>
+            For persistence, the Data Mapper pattern was chosen.  Domain model objects can be constructed from database
+            entries extracted using a dictionary of classes mapped to a certain type, so that a common interface can
+            be used to retrieve, save and update persisted objects.  It would have been preferable to use stored procedures
+            rather than hard coding SQL statements, but the <code>CREATE PROCEDURE</code> statement was denied.
+        </p>
+        <h3>Issues Overcome</h3>
+        <p>
+            As PEAR libraries were not allowed to be used, an XML serialiser needed to be devised.  Rather than creating
+            a class to serailise each object into XML, a class was created that reflected the properties on an object 
+            instance and produced an XML file.
+        </p>
+        <p>
+            In terms of server-side caching, data-caching would have been far more appropriate for this assignment, but
+            as this requires libraries such as <a href="http://php.net/memcache" target="_blank">memcache</a> are not 
+            available on the UWE Apache servers.  Instead a content caching mechanism was devised where the contents
+            of the the output buffer are automatically saved for each request and retrieved if a path previously accessed
+            has been cached.
+        </p>
+        <h3>Learning Outcomes</h3>
+        <p>
+            I learnt a great deal about utilising design patterns in a functional programming language (PHP), and was able
+            to expand my knowledge of creating RESTful data APIs that conform to a predefined structure.
+        </p>
     </div>
 </div>
 <div class="row">
